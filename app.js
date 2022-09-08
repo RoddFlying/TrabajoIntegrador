a77797a66af80f15680a8440a03b92a96063b15b
maureenparody <maureenparody@gmail.com>
2022-09-08 13:36:40 -0400

    express instalado, no hace falta q instalen

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.use(express.static(path.join( dirname, '/public'))) ;
-
-app.listen(3000,()=>{
-    console.log("Esta en el puerto 3000");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get('/', (req,res) =>{
-res. send("Cambie"); // permite enviar texto o codlgo
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
