ba8cac37e8ef86094b23b1a9526f3babe505faed
maureenparody <maureenparody@gmail.com>
2022-09-08 12:51:37 -0400

    express en puerto 3000

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static(path.join( dirname, '/public'))) ;
 
+app.listen(3000,()=>{
+    console.log("Esta en el puerto 3000");
+})
+
 app.get("/', (req,res) =>
 res. send("Cambie"): // permite enviar texto o codlgo
 1);
