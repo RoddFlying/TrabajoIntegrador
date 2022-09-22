0a36dfb6faa47fd0fd6554ab4be0d4d779a7e764
Rodd <rodrigoac3vedo@gmail.com>
2022-09-22 14:43:31 -0300

    App.js

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ app.get('/login', function (req, res) {
   res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
-app.get('/login', function (req, res) {
+app.get('/contact', function (req, res) {
   res.sendFile(path.resolve(__dirname, './views/contact.html'));
 });
 
-app.get('/login', function (req, res) {
+app.get('/cart', function (req, res) {
   res.sendFile(path.resolve(__dirname, './views/cart.html'));
 });
 
-app.get('/login', function (req, res) {
+app.get('/product', function (req, res) {
   res.sendFile(path.resolve(__dirname, './views/product.html'));
 });
 
