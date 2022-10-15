6e3b302b2e8ea2295dafc837d95332c42072d8da
ProgramacionDH <marcelocanay@hotmail.com>
2022-10-15 20:37:17 -0300

    multer en mainRouter

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,6 +1,7 @@
 const mainControllers = require("../controllers/mainControllers");
 const express = require("express");
 const router = express.Router();
+const multer = require ("multer")
 router.get("/", mainControllers.index);
 
 module.exports = router;
