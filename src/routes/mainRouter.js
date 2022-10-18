9dd7b481ff34961ad444e228c5326fd21f868e08
Rodd <rodrigoac3vedo@gmail.com>
2022-10-18 17:38:04 -0300

    Multer Front Rodd

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const multer = require ('multer');
+
 router.get("/", mainControllers.index);
 const mainControllers = require("../controllers/mainControllers");
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
+const multer = require ('multer'); //Multer//
+
+// Requerimiento del controller //
+
 const productsController = require('../controllers/productsController')
+
+//Multer Configuracion //
+
+const ConfiguracionImagen = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname,  '../../public/images/products'));
+    },
+    filename: function(req, file, cb) {
+        let imageName = Date.now() + file.originalname ;
+        cb (null,imageName);
+    }
+});
+
+// Carrito //
 
 router.get('/carrito', productsController.carrito)
 
-//router.get('/detalleproducto', productosController.detalle)
-//router.get('/crearproducto', productosController.crear)
+//Get all products //
+
+router.get('/', productosController.home);
+
+// Create a product // 
+
+router.get('/createProducts', productsController.create);
+router.post('/createProducts' , uploadFile.single('imageProduct'), productsController.store);
+
+// Get a product //
+
+router.get("/detail/:id", productsController.detail);
+
+//Edit a product //
+
+router.get("/detail/:id", productsController.edit);
+router.put("edit/:id", productsController.update);
+
+//Delete product //
+
+router.delete("/:id", productsController.destroy);
 
 module.exports = router;
