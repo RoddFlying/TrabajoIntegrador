7bce2ece93d6073cf6b5012a68f3bc66a515fd20
Rodd <rodrigoac3vedo@gmail.com>
2022-10-18 17:59:29 -0300

    Multer Rodd

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,4 @@
+const { fstat } = require("fs");
 const path = require("path");
 
 const controlador = {
@@ -8,10 +9,34 @@ const controlador = {
    
   },
 };
+//Create a product //
 
 store: (req, res) => {
-  console.log(req.file);
+  idNuevo = 0;
+  for (let s of products){
+    if (idNuevo < s.id){
+       idNuevo=s.id;
+    }
+  }
+  idNuevo++;
+
+  let nombreImagen = req.file.filename;
+
+  let productoNuevo = {
+    id: idNuevo,
+    name: req.body.name ,
+    price: req.body.price ,
+    discount: req.body.discount,
+    category: req.body.categorym,
+    description: req.body.description,
+    iamge: nombreImagen
+  };
+
+  products.push(productoNuevo);
   
+  fs.writeFileSync(productsFilePath, JSON.stringify(products,null,' '));
+
+  res.redirect('/');
 };
 
 module.exports = controlador;
