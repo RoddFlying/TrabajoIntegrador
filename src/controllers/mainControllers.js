c8c6db7815da4ce66ad7592d5825926ccc211158
Rodd <rodrigoac3vedo@gmail.com>
2022-10-18 18:45:22 -0300

    Multer Rodd

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,6 +1,22 @@
+const fs = require('fs');
+const path = require ('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+
+
 const controlador = {
-  index: (req, res) => {
-    res.render("./home");
+  index: (req, res) => { 
+
+    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+    res.render("./home", {products: products});
+
+  },
+  search: (req,res)=>{
+
   },
 };
 module.exports = controlador;
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,4 +1,4 @@
-const { fstat } = require("fs");
+const fs  = require("fs");
 const path = require("path");
 
 const controlador = {
