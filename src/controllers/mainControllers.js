2bdc50ba478124f5fe1898357d2a13ea1d251b2a
Rodd <rodrigoac3vedo@gmail.com>
2022-10-19 00:40:34 -0300

    Rod

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -5,8 +5,6 @@ const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 
-
-
 const controlador = {
   index: (req, res) => { 
 
