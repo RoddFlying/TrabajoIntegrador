09d8b11602593afd7c8358474925730479ffcce4
maureenparody <maureenparody@gmail.com>
2022-10-19 23:29:01 -0300

    css y camvios a creatreproducts

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -4,5 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/login", usersController.login); //USERS MODIFICACION
+// router.get("/register", usersController.register); 
+// router.get("/profile", usersController.profile); 
 
 module.exports = router;
