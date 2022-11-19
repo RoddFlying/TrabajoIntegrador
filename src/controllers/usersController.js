03358d404435236d13a1a77a1adcf2e2ffb7b299
Rodd <rodrigoac3vedo@gmail.com>
2022-11-18 23:08:22 -0300

    video terminado

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -54,7 +54,7 @@ processRegister: (req,res) => {
        req.session.userLogged = userToLogin;
 
        if (req.body.remember_user){
-        res.cookie('userEmail', req.body.email, {maxAge: (1000 * 60) *2})
+        res.cookie('userEmail', req.body.email, {maxAge: (1000 * 60) * 60 })
        }
        return res.redirect('/user/profile');
    }
