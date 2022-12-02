23b82d9230e22649c0e371a0ab19c88831ea091f
maureenparody <maureenparody@gmail.com>
2022-12-01 21:26:10 -0300

    products.js

--- src/database/models/products.js
+++ src/database/models/products.js
@@ -1,55 +1,58 @@
-module.exports = (sequelize,dataType)=>{
-    const alias = 'products';
-    const cols = {
+const {sequelize,DataTypes} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    const Product = sequelize.define('Product', {
         id:{
-            type: dataType.INTEGER,
+            type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncremental: true
         },
         name: {
-            type: dataType.STRING(100),
+            type: DataTypes.STRING(100),
             allowNull: false
         },
         brand: {
-            type: dataType.STRING(45),
+            type: DataTypes.STRING(45),
             allowNull: true
         },
         detail: {
-            type: dataType.STRING(10000),
+            type: DataTypes.STRING(10000),
             allowNull: true
         },
         price: {
-            type: dataType.DECIMAL(65,00),
+            type: DataTypes.DECIMAL(65,00),
             allowNull: false
         },
         discount: {
-            type: dataType.INTEGER(100),
+            type: DataTypes.INTEGER(100),
             allowNull: true
         },
         image: {
-            type: dataType.STRING(200),
+            type: DataTypes.STRING(200),
             allowNull: false
         },
         expiration_date: {
-            type: dataType.DATE,
+            type: DataTypes.DATE,
             allowNull: true
         },
         creation_date: {
-            type: dataType.DATE,
+            type: DataTypes.DATE,
             allowNull: true
         },
         delete_date: {
-            type: dataType.DATE,
+            type: DataTypes.DATE,
             allowNull: true
         },
         product_Category_id: {
-            type: dataType.INTEGER,
+            type: DataTypes.INTEGER,
             allowNull: false
         },
         supplier_id: {
-            type: dataType.INTEGER,
+            type: DataTypes.INTEGER,
             allowNull: true
         }
 
-    };
+    })
+    return Product;
 }
+
