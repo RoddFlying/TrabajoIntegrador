453dbf78a139ca804c465cda715821461db48bf6
maureenparody <maureenparody@gmail.com>
2022-12-04 19:15:52 -0300

    confir bd

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,27 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "mauchisss",
-    "password": "Frida2020!",
-    "database": "mauchisss_growshop",
-    "host": "mysql-mauchisss.alwaysdata.net", //revisar si esta bien conectado a mamp
-    "dialect": "mysql",
-    "port": 3306
-  },
-  "test": {
-    "username": "mauchisss",
-    "password": "Frida2020",
-    "database": "database_test",
-    "host": "mysql-mauchisss.alwaysdata.net",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "mauchisss",
-    "password": "Frida2020",
-    "database": "database_production",
-    "host": "mysql-mauchisss.alwaysdata.net",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST, //revisar si esta bien conectado a mamp
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT
   }
+  //,
+  // "test": {
+  //   "username": "mauchisss",
+  //   "password": "Frida2020",
+  //   "database": "database_test",
+  //   "host": "mysql-mauchisss.alwaysdata.net",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": "mauchisss",
+  //   "password": "Frida2020",
+  //   "database": "database_production",
+  //   "host": "mysql-mauchisss.alwaysdata.net",
+  //   "dialect": "mysql"
+  // }
 }
