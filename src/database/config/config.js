6b8cfeb0880aef25bf751f4cfc3804a7b0010acc
maureenparody <maureenparody@gmail.com>
2022-12-04 17:49:24 -0300

    conexion a bdr

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,17 +8,17 @@ module.exports = {
     "port": 3306
   },
   "test": {
-    "username": "root",
-    "password": null,
+    "username": "mauchisss",
+    "password": "Frida2020",
     "database": "database_test",
-    "host": "127.0.0.1",
+    "host": "mysql-mauchisss.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
+    "username": "mauchisss",
+    "password": "Frida2020",
     "database": "database_production",
-    "host": "127.0.0.1",
+    "host": "mysql-mauchisss.alwaysdata.net",
     "dialect": "mysql"
   }
 }
