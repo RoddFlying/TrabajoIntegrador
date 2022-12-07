eeb6a74c19ac3aad1a1aad8d061f3bfa5a58a56d
Rodd <rodrigoac3vedo@gmail.com>
2022-12-07 17:20:00 -0300

    asd

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,20 +8,5 @@ module.exports = {
     "host": process.env.DB_HOST, //revisar si esta bien conectado a mamp
     "dialect": process.env.DB_DIALECT,
     "port": process.env.DB_PORT
-  }
-  //,
-  // "test": {
-  //   "username": "mauchisss",
-  //   "password": "Frida2020",
-  //   "database": "database_test",
-  //   "host": "mysql-mauchisss.alwaysdata.net",
-  //   "dialect": "mysql"
-  // },
-  // "production": {
-  //   "username": "mauchisss",
-  //   "password": "Frida2020",
-  //   "database": "database_production",
-  //   "host": "mysql-mauchisss.alwaysdata.net",
-  //   "dialect": "mysql"
-  // }
+  }  
 }
