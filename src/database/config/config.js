84f7de5dcf2b278f93071e2e4811ab19a072ab7c
maureenparody <maureenparody@gmail.com>
2022-12-08 14:24:50 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -9,19 +9,21 @@ module.exports = {
     "dialect": process.env.DB_DIALECT,
     "port": process.env.DB_PORT
   }
-  //,
-  // "test": {
-  //   "username": "mauchisss",
-  //   "password": "Frida2020",
-  //   "database": "database_test",
-  //   "host": "mysql-mauchisss.alwaysdata.net",
-  //   "dialect": "mysql"
-  // },
-  // "production": {
-  //   "username": "mauchisss",
-  //   "password": "Frida2020",
-  //   "database": "database_production",
-  //   "host": "mysql-mauchisss.alwaysdata.net",
-  //   "dialect": "mysql"
-  // }
+  ,
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST, //revisar si esta bien conectado a mamp
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST, //revisar si esta bien conectado a mamp
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT
+  }
 }
