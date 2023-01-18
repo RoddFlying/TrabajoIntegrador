2dc045dc0aa2a3528a96f18b7dbabf820fba9207
maureenparody <maureenparody@gmail.com>
2023-01-17 21:36:24 -0300

    vistas

--- public/js/validations/addItemCart.js
+++ public/js/validations/addItemCart.js
@@ -2,7 +2,7 @@ const Swal = require('sweetalert2');
 
 boton.addEventListener('click', function(){
     let nombreProducto = document.getElementsByName('name');
-    let precioProducto = document.getElementsByName('price');
+    let precioProducto = document.getElementsByName('price');//precio c descuento
     let productoNuevo = {nombre: nombreProducto, precio: precioProducto};
     let productosCarrito = JSON.parse(localStorage.getItem('carrito'));
     productosCarrito.push(productoNuevo);
