ae80448b68cdd6fbe4e92a7850b966bb37a03c11
maureenparody <maureenparody@gmail.com>
2023-01-18 18:18:53 -0300

    arrancando con el cod para el carrito

--- public/js/validations/addItemCart.js
+++ public/js/validations/addItemCart.js
@@ -1,10 +1,10 @@
 const Swal = require('sweetalert2');
 
-boton.addEventListener('click', function(){
+boton.addEventListener('click', ()=>{
     let nombreProducto = document.getElementsByName('name');
     let precioProducto = document.getElementsByName('finalPrice');
     let imagenProducto = document.getElementsByName('image');
-    let productoNuevo = {nombre: nombreProducto, precio: precioProducto};
+    let productoNuevo = {nombre: nombreProducto, precio: precioProducto, imagen:imagenProducto};
     let productosCarrito = JSON.parse(localStorage.getItem('carrito'));
     productosCarrito.push(productoNuevo);
     localStorage.setItem('carrito',JSON.stringify(productosCarrito))
--- public/js/validations/showItemsCart.js
+++ public/js/validations/showItemsCart.js
@@ -1,2 +1,7 @@
 let productosCarrito = JSON.parse(localStorage.getItem('carrito'));
 //agregar for para mostrar
+
+// productosCarrito.forEach(product => {
+ 
+// });
+const total = productosCarrito.reduce((acc, items)=> acc + items.precio, 0 )
