b0f53d07d1ad3b2adadf589c4a1707d65a47189b
maureenparody <maureenparody@gmail.com>
2023-01-18 15:55:31 -0300

    no se porque se cambio al puerto 3001

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
 //listen//
 
 app.listen(3001, () => {
-  console.log("servidor corriendo puerto 3000");
+  console.log("servidor corriendo puerto 3001");
 });
 
 // exports
