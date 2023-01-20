25d34a532af619abbafce501b502d59e25c0fef2
maureenparody <maureenparody@gmail.com>
2023-01-19 21:38:38 -0300

    cambios carrito

--- public/js/validations/addItemCart.js
+++ public/js/validations/addItemCart.js
@@ -1,13 +1,20 @@
 const Swal = require('sweetalert2');
 
+let boton = document.getElementById('agregarcarrito');
+
 boton.addEventListener('click', ()=>{
     let nombreProducto = document.getElementsByName('name');
     let precioProducto = document.getElementsByName('finalPrice');
     let imagenProducto = document.getElementsByName('image');
     let productoNuevo = {nombre: nombreProducto, precio: precioProducto, imagen:imagenProducto};
     let productosCarrito = JSON.parse(localStorage.getItem('carrito'));
+    if (productosCarrito == undefined || productosCarrito == null){
+        productosCarrito = []
+    }
     productosCarrito.push(productoNuevo);
     localStorage.setItem('carrito',JSON.stringify(productosCarrito))
+    alert('producto se sumo al carrito');
 }) 
+
 //¿Como exportar el carrito?
 //module.exports = productosCarrito;
