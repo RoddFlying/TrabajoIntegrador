c33264ef91b6b5865960e54d8ad454a85840f3d9
maureenparody <maureenparody@gmail.com>
2023-01-20 19:18:48 -0300

    funciona cart y logo en todos

--- public/js/addItemCart.js
+++ public/js/addItemCart.js
@@ -5,13 +5,16 @@ boton.addEventListener('click', ()=>{
     let nombreProducto = document.getElementById('name').innerText;
     let precioProducto = document.getElementById('price').innerText;
     let descuentoProducto = document.getElementById('discount').innerText;
+    if (descuentoProducto == undefined || descuentoProducto == null){
+        descuentoProducto = 00;
+    }
     let precioFinalProducto = document.getElementById('finalprice').innerText;
-    let imagenProducto = document.getElementById('image').innerText;
-    let productoNuevo = {nombre: nombreProducto, precio: precioProducto, imagen:imagenProducto, descuento: descuentoProducto, precioFinal: precioFinalProducto};
+    let productoNuevo = {nombre: nombreProducto, precio: precioProducto, descuento: descuentoProducto, precioFinal: precioFinalProducto};
     let productosCarrito = JSON.parse(localStorage.getItem('carrito'));
     if (productosCarrito == undefined || productosCarrito == null){
         productosCarrito = []
     }
+    
     productosCarrito.push(productoNuevo);
     localStorage.setItem('carrito',JSON.stringify(productosCarrito))
     alert('producto se sumo al carrito');
--- public/js/showItemsCart.js
+++ public/js/showItemsCart.js
@@ -1,10 +1,8 @@
-alert('ver los items');
-
 let productosCarrito = JSON.parse(localStorage.getItem('carrito'));
 
 console.log(productosCarrito);
 
-const total = productosCarrito.reduce((acc, items)=> acc + items.precioFinal, 0 )
+//const total = productosCarrito.reduce((acc, items)=> acc + items.precioFinal, 0 )
 
 let div1 = document.getElementById('itemscart');
 
@@ -12,26 +10,21 @@ let div2 = document.getElementById('totalcart');
 
 for (let p of productosCarrito){
 
-    // let img1 = document.createElement('img');
-    // img1.innerText = p.imagen;
-    // div1.appendChild(img1);
+    let nomb = document.createElement('h2');
+    nomb.innerText = p.nombre;
+    div1.appendChild(nomb);
 
-    let h21 = document.createElement('h2');
-    h21.innerText = p.nombre;
-    div1.appendChild(h21);
+    let precio = document.createElement('h3');
+    precio.innerText = p.precio;
+    div1.appendChild(precio);
 
-    // let h31 = document.createElement('h3');
-    // h31.innerText = p.precio;
-    // div1.appendChild(h31);
+    let disc = document.createElement('h4');
+    disc.innerText = p.descuento;
+    div1.appendChild(disc);
 
-    // let h32 = document.createAttribute('h3');
-    // h32.innerText = p.descuento;
-    // div1.appendChild(h32);
-
-    // let h22 = document.createAttribute('h2');
-    // h22.innerText = total;
-    // div2.appendChild(h22);
-    
+    let precioProd = document.createElement('h3');
+    precioProd.innerText = p.precioFinal;
+    div1.appendChild(precioProd);
     
 }
 
