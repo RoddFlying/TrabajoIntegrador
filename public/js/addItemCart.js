c1542c4e2cc5f3ecff4400ae7622239a2474a087
maureenparody <maureenparody@gmail.com>
2023-01-20 18:17:11 -0300

    no veo donde esta mal el inner text

--- public/js/addItemCart.js
+++ public/js/addItemCart.js
@@ -2,11 +2,11 @@
 var boton = document.getElementById('agregarcarrito');
 
 boton.addEventListener('click', ()=>{
-    let nombreProducto = document.getElementsByName('name');
-    let precioProducto = document.getElementsByName('price');
-    let descuentoProducto = document.getElementsByName('discount');
-    let precioFinalProducto = document.getElementsByName('finalprice')
-    let imagenProducto = document.getElementsByName('image');
+    let nombreProducto = document.getElementById('name').innerText;
+    let precioProducto = document.getElementById('price').innerText;
+    let descuentoProducto = document.getElementById('discount').innerText;
+    let precioFinalProducto = document.getElementById('finalprice').innerText;
+    let imagenProducto = document.getElementById('image').innerText;
     let productoNuevo = {nombre: nombreProducto, precio: precioProducto, imagen:imagenProducto, descuento: descuentoProducto, precioFinal: precioFinalProducto};
     let productosCarrito = JSON.parse(localStorage.getItem('carrito'));
     if (productosCarrito == undefined || productosCarrito == null){
@@ -15,7 +15,8 @@ boton.addEventListener('click', ()=>{
     productosCarrito.push(productoNuevo);
     localStorage.setItem('carrito',JSON.stringify(productosCarrito))
     alert('producto se sumo al carrito');
+    console.log(productosCarrito);
 }) 
-console.log(productosCarrito);
+
 //¿Como exportar el carrito?
 //module.exports = productosCarrito;
