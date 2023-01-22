e388b7d9b3115db19adc313b53ba1c5f35bff2bd
maureenparody <maureenparody@gmail.com>
2023-01-22 00:19:08 -0300

    todavia salta permisos user

--- public/js/showAdmin.js
+++ public/js/showAdmin.js
@@ -1,13 +1,10 @@
 const db = require('../database/models');
 
-let div1 = document.getElementById('funcionesAdmin');
 
-
-
-    if (db.role_id =='1'){
-        document.getElementById('funcionesAdmin').style.display = block;
+    if (db.user.role_id == 1){
+        document.getElementById('funcionesAdmin').style.display = "block";
     } else {
-        document.getElementById('funcionesAdmin').style.display = none;
+        document.getElementById('funcionesAdmin').style.display = "none";
     }
     
 
